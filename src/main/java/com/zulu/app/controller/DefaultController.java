d2d96d369a49814b91b6562461c8490ddee42b0c
Luke <80142097+LukeRykta@users.noreply.github.com>
2025-08-01 17:15:49 -0500

    add log.info test

--- src/main/java/com/zulu/app/controller/DefaultController.java
+++ src/main/java/com/zulu/app/controller/DefaultController.java
@@ -1,13 +1,18 @@
 package com.zulu.app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DefaultController {
 
+    private static final Logger log = LoggerFactory.getLogger(DefaultController.class);
+
     @GetMapping("/hello")
     public String hello() {
+        log.info("first log test");
         return "Hello, Spring Boot!";
     }
 }
